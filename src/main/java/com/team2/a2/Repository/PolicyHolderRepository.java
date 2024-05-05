cf3e43c0bdb1604879bfb0ea22564528407a9123
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-05-05 10:45:28 +0700

    feat!: getPolicyHolderByAccountId implemented

---
+++ src/main/java/com/team2/a2/Repository/PolicyHolderRepository.java
@@ -0,0 +1,55 @@
+package com.team2.a2.Repository;
+
+import com.team2.a2.ConnectionManager;
+import com.team2.a2.Model.Enum.AccountType;
+import com.team2.a2.Model.User.Account;
+import com.team2.a2.Model.User.Customer.PolicyHolder;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class PolicyHolderRepository {
+
+    private Connection connection;
+    public PolicyHolderRepository() {
+        this.connection = ConnectionManager.getConnection();
+    }
+
+    public PolicyHolder getPolicyHolderByAccountId(int accountId) {
+        PolicyHolder policyHolder = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            String sql = "SELECT * FROM policyholders WHERE account_id = ?";
+            statement = connection.prepareStatement(sql);
+            statement.setInt(1, accountId);
+            resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                // If policyHolder found, create a Policyholder object
+                int id = resultSet.getInt("id");
+                Date createdAt = resultSet.getDate("created_at");
+                Date updatedAt = resultSet.getDate("updated_at");
+                int policyOwnerId = resultSet.getInt("policyowner_id");
+                String name = resultSet.getString("name");
+                String homeAddress = resultSet.getString("home_address");
+                String phoneNumber = resultSet.getString("phone_number");
+                String email = resultSet.getString("email");
+
+                policyHolder = new PolicyHolder(id, createdAt, updatedAt, accountId, policyOwnerId, name, homeAddress, phoneNumber, email); // TODO: create a policy holder object based on the result.
+
+                return policyHolder;
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error fetching policyholder: " + e.getMessage());
+        }
+
+        return policyHolder;
+    }
+
+}
