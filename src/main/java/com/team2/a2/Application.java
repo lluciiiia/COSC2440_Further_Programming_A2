c5fead3754dff492e5154fc36f7f5d99704747b4
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-05-05 11:12:04 +0700

    chore: delete testing code in Application.java

--- src/main/java/com/team2/a2/Application.java
+++ src/main/java/com/team2/a2/Application.java
@@ -1,8 +1,5 @@
 package com.team2.a2;
 
-import com.team2.a2.Controller.AccountController;
-import com.team2.a2.Model.Enum.AccountType;
-import com.team2.a2.Request.LoginRequest;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -20,9 +17,6 @@ public class Application extends javafx.application.Application {
         // Load the FXML file and create the scene
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-
-        AccountController accountController = new AccountController();
-        accountController.login(new LoginRequest("s3939114@rmit.edu.vn", "12345"));
 
         // Set up the stage
         stage.setTitle("Hello!");
