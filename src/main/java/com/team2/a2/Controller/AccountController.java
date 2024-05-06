1123913bbbab64734d7011e5ddfd44ea908658ca
Phan Nam Nguyen <73817858+nguyenphannam2223@users.noreply.github.com>
2024-05-07 00:12:20 +0700

    fix: delete wrong commits

--- src/main/java/com/team2/a2/Controller/AccountController.java
+++ src/main/java/com/team2/a2/Controller/AccountController.java
@@ -2,6 +2,8 @@ package com.team2.a2.Controller;
 
 import com.team2.a2.Facade.AccountFacade;
 import com.team2.a2.FacadeImpl.AccountFacadeImpl;
+import com.team2.a2.Model.Enum.AccountType;
+import com.team2.a2.Model.User.Account;
 import com.team2.a2.Request.LoginRequest;
 
 public class AccountController {
@@ -14,5 +16,14 @@ public class AccountController {
 
     public boolean login(LoginRequest request) {
         return accountFacade.login(request);
+    }
+
+    public AccountType getAccountType(String username, String password) {
+        Account account = accountFacade.getAccount(username, password);
+        if (account != null) {
+            return account.getType();
+        } else {
+            return null;
+        }
     }
 }
---
+++ src/main/java/com/team2/a2/DependentClaimView.java
@@ -0,0 +1,4 @@
+package com.team2.a2;
+
+public class DependentClaimView {
+}
