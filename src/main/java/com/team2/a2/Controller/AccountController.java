1f5eb40195b692698ae1834d581c7fc7b6423194
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-05-14 05:19:21 +0700

    fix!: testLogin DB connection

--- src/main/java/com/team2/a2/Controller/AccountController.java
+++ src/main/java/com/team2/a2/Controller/AccountController.java
@@ -13,10 +13,6 @@ public class AccountController {
         this.accountFacade = new AccountFacadeImpl();
     }
 
-    public AccountController(AccountFacade accountFacade) {
-        this.accountFacade = accountFacade;
-    }
-
     public Account login(LoginRequest request) { return accountFacade.login(request); }
 
     public Account getAccountByID(int accountID) {
--- src/test/java/org/example/AccountControllerTest.java
+++ src/test/java/org/example/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.team2.a2.ConnectionManager;
 import com.team2.a2.Controller.AccountController;
 import com.team2.a2.Facade.AccountFacade;
 import com.team2.a2.Model.Enum.AccountType;
@@ -8,11 +9,10 @@ import com.team2.a2.Request.LoginRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 import java.sql.Date;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 public class AccountControllerTest {
@@ -24,40 +24,38 @@ public class AccountControllerTest {
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        accountController = new AccountController(mockAccountFacade);
+        ConnectionManager.initConnection();
+        accountController = new AccountController(); // No need to pass mockAccountFacade here if you're not using it
     }
 
     @Test
     public void testLogin() {
-        // Arrange
+
         LoginRequest loginRequest = new LoginRequest("im_dependent", "12345");
-        Account expectedAccount = new Account(1, Date.valueOf("2024-05-05"), Date.valueOf("2024-05-05"),
-                "im_dependent", "12345", AccountType.DEPENDENT);
+        Account actualTrueAccount = accountController.login(loginRequest);
 
-        // Mock behavior of AccountFacade
-        when(mockAccountFacade.login(loginRequest)).thenReturn(expectedAccount);
+        // True Case
+        assertSame(2, actualTrueAccount.getId());
 
-        // Act
-        Account actualAccount = accountController.login(loginRequest);
+        // False Case
+        assertNotSame(3, actualTrueAccount.getId());
 
-        // Assert
-        assertEquals(expectedAccount, actualAccount);
     }
 
-//    @Test
-//    public void testGetAccountByID() {
-//        // Arrange
+    @Test
+    public void testGetAccountByID() {
+        // Arrange
 //        int accountID = 1;
-//        Account expectedAccount = new Account(accountID, "username", AccountType.CUSTOMER);
+//        Account expectedAccount = new Account(accountID, Date.valueOf("2024-05-05"), Date.valueOf("2024-05-05"),
+//                "im_dependent", "12345", AccountType.DEPENDENT);
 //
 //        // Mock behavior of AccountFacade
-//        when(mockAccountFacade.getAccountByID(accountID)).thenReturn(expectedAccount);
+//        when(accountController.getAccountByID(accountID)).thenReturn(expectedAccount);
 //
 //        // Act
 //        Account actualAccount = accountController.getAccountByID(accountID);
 //
 //        // Assert
 //        assertEquals(expectedAccount, actualAccount);
-//    }
+    }
 }
