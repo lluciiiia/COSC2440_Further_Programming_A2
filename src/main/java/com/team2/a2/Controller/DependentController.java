58c1320863c87ec47b9d9bce128c19079040a61f
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-05-15 16:14:15 +0700

    fix!: create a DependentController constructor

--- src/main/java/com/team2/a2/Controller/DependentController.java
+++ src/main/java/com/team2/a2/Controller/DependentController.java
@@ -1,11 +1,16 @@
 package com.team2.a2.Controller;
 
 import com.team2.a2.Facade.DependentFacade;
+import com.team2.a2.FacadeImpl.DependentFacadeImpl;
 import com.team2.a2.Model.User.Customer.Dependent;
 
 public class DependentController {
 
     private DependentFacade dependentFacade;
+
+    public DependentController() {
+        this.dependentFacade = new DependentFacadeImpl();
+    }
 
     public Dependent getDependentByCustomerId(int id) {
         return dependentFacade.getDependentByCustomerId(id);
