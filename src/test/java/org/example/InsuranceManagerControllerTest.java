5cdf884b4773abf81b901419d527a3a4b61b899f
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-05-15 17:05:20 +0700

    feat!: testGetAllInsuranceSurveyors

--- src/test/java/org/example/InsuranceManagerControllerTest.java
+++ src/test/java/org/example/InsuranceManagerControllerTest.java
@@ -15,7 +15,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-
 public class InsuranceManagerControllerTest {
 
     private InsuranceManagerController insuranceManagerController;
