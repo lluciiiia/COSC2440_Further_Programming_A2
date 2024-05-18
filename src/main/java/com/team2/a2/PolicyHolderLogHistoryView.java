92a31e97552ebcadbb6471666ae1d531a773dd5f
minhquan <s3938194@rmit.edu.vn>
2024-05-18 23:04:21 +0700

    feat: view log history for all roles except dependent

---
+++ src/main/java/com/team2/a2/PolicyHolderLogHistoryView.java
@@ -0,0 +1,45 @@
+package com.team2.a2;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class PolicyHolderLogHistoryView implements Initializable {
+    @FXML
+    private Button returnButton;
+//    @FXML
+//    private TableColumn<History, double> idColumn;
+//    @FXML
+//    private TableColumn<History, double> userIdColumn;
+//    @FXML
+//    private TableColumn<History, String> roleColumn;
+//    @FXML
+//    private TableColumn<History, String> logHistoryColum;
+
+
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        returnButton.setOnAction(event -> {
+            try {
+                Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("PolicyHolderPage.fxml")));
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) returnButton.getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
