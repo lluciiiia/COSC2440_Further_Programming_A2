bc1dab0776495a25e4e4b3ef615342852655ab10
Lucia (Seokyung Kim) <0617lucia@gmail.com>
2024-05-18 17:25:40 +0700

    feat: createLog query implemented

--- src/main/java/com/team2/a2/Repository/LogRepository.java
+++ src/main/java/com/team2/a2/Repository/LogRepository.java
@@ -2,7 +2,7 @@ package com.team2.a2.Repository;
 
 import com.team2.a2.ConnectionManager;
 
-import java.sql.Connection;
+import java.sql.*;
 
 public class LogRepository {
 
@@ -13,5 +13,31 @@ public class LogRepository {
     }
 
 
+    public void createLog(int accountId, String content) {
+        PreparedStatement statement = null;
 
+        try {
+            String sql = "INSERT INTO logs (account_id, content) " +
+                    "VALUES (?, ?)";
+            statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, accountId);
+            statement.setString(2, content);
+
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("A new log was inserted successfully!");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error creating log: " + e.getMessage());
+        } finally {
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    System.err.println("Error closing PreparedStatement: " + e.getMessage());
+                }
+            }
+        }
+    }
 }
