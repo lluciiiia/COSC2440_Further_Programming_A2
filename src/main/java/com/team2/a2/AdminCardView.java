df12eef0ec3fccba79a8b3f434eb9f576c39b56a
cuducquang <112458703+cuducquang@users.noreply.github.com>
2024-05-18 17:18:23 +0700

    view Customer insuranceCard for Admin and manager

---
+++ src/main/java/com/team2/a2/AdminCardView.java
@@ -0,0 +1,56 @@
+package com.team2.a2;
+
+import com.team2.a2.Model.InsuranceObject.InsuranceCard;
+import com.team2.a2.Model.User.Account;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminCardView implements Initializable {
+    @FXML
+    private Button returnButton;
+    @FXML
+    private TextField customerID;
+    @FXML
+    private TextField cardNumber;
+    @FXML
+    private TextField ExpiryDate;
+    @FXML
+    private TextField BankName;
+    @FXML
+    private TextField BankAccountNumber;
+
+
+    public void initData(InsuranceCard insuranceCard) {
+        customerID.setText("CustomerID: " + insuranceCard.getCustomerId());
+        cardNumber.setText("Card number: " + insuranceCard.getCardNumber());
+        ExpiryDate.setText("Expiry date: " + insuranceCard.getExpiryDate());
+        BankName.setText("Bank name: " + insuranceCard.getBankName());
+        BankAccountNumber.setText("Bank account number: " + insuranceCard.getAccountNumber());
+    }
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        returnButton.setOnAction(event -> {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("AdminPage.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) returnButton.getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
